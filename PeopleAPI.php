8d4a89f651d54048bd921eb6eb83ff1c61a9008a
AppHelloWorld <sebassfn@gmail.com>
2016-11-26 13:09:29 -0500

    Modificacion PeopleAPI.php

--- PeopleAPI.php
+++ PeopleAPI.php
@@ -1,5 +1,5 @@
 <?php
-include './PeopleDB.php'; //Incluyo el archivo 'PeopleDB.php'
+require_once './PeopleDB.php'; //Incluyo el archivo 'PeopleDB.php'
 
 /*
  * To change this license header, choose License Headers in Project Properties.
